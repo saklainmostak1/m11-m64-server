8c8879e420e4744396df0eecff83a8c0204f54cc
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-30 01:35:31 +0600

    push

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req, res) =>{
+    res.send('api is running')
+} )
+
+
+
+
+
+
+app.listen(port,() =>{
+    console.log(`lisent port ${port}`)
+} )
